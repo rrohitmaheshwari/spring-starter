aec3ddceb0b699b530bf66649f29577613a30112
Rohit Maheshwari <rohit.maheshwari@philips.com>
2019-10-10 23:52:36 -0700

    Update ProjectApplication.java

--- src/main/java/com/example/starter/project/ProjectApplication.java
+++ src/main/java/com/example/starter/project/ProjectApplication.java
@@ -3,7 +3,7 @@ package com.example.starter.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class ProjectApplication {
 
     public static void main(String[] args) {
